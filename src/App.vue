2da7a59857e70ec934b517c444a4d07d7e2b727b
chenketaowb614 <chenketaowb614@hellobike.com>
2021-08-12 16:27:27 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="App" >
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  .App{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    background:red;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#1890ff"
+    >
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-menu-item index="2">处理中心</el-menu-item>
+      <el-menu-item index="3">处理中心</el-menu-item>
+      <el-menu-item index="4">处理中心</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
