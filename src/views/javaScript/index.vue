dc46c3053898f3d2fdd6f52f5e9a687b9c2783fb
mh <596796585@qq.com>
2022-03-01 13:44:11 +0800

    vue项目

--- src/views/javaScript/index.vue
+++ src/views/javaScript/index.vue
@@ -1,17 +1,23 @@
 <template>
-  <div >
-
-      1111
+  <div>
+    <div>{{ a.b }}</div>
   </div>
 </template>
 
 <script>
-
 export default {
-
+  data () {
+    return {
+      a: {}
+    }
+  },
+  created () {
+    this.a.b = 1
+  },
+  mounted () {
+    this.a.b = 2
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
